749dbd996131ee2e596f129a5d0ff3fb5d4c54cd
Tenes <enzo.bruni@aol.fr>
2020-06-27 00:52:36 +0200

    WIP

---
+++ Code/Global/FirebaseAPI.cs
@@ -0,0 +1,20 @@
+// using Godot;
+// using System;
+// using System.Net.Http;
+// using System.Text;
+// using System.Threading.Tasks;
+
+///POST PONED
+public static class FirebaseAPI
+{
+    // private static string _apiKey = "AIzaSyBNi94YYSiqI0wdqVz0A6tiYssgHoBHRmQ";
+    // private static readonly HttpClient _client = new HttpClient();
+    // public static async Task<bool> OAuthGoogle()
+    // {
+    //     string endpoint = $"https://identitytoolkit.googleapis.com/v1/accounts:signInWithIdp?key={_apiKey}";
+    //     string body = $"{\"postBody\":\"id_token=[GOOGLE_ID_TOKEN]&providerId=[google.com]\",\"requestUri\":\"[http://localhost]\",\"returnIdpCredential\":true,\"returnSecureToken\":true}'";
+    //     var data = new StringContent(json, Encoding.UTF8, "application/json");
+    //     await _client.PostAsync(endpoint, );
+    // }
+
+}
